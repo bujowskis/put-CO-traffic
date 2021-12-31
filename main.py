0717ba7491e109f03eb5f335cc3c7c3e758b6a4a
bujowskis <s.bujowski@gmail.com>
2021-12-31 19:33:15 +0100

    add small test in main

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
-import inputreader
-import objects as obj
-import instance as ins
+import inputreader3
+import objects3 as obj
+import instance3 as ins
+import time
 
 # main program
 # todo
 
 
 def main():
+    # fixme - see the differences; greedy acts concerning
+    # firstly, we load the instance of a problem from a file
+    files = ["a.txt", "b.txt", "c.txt", "d.txt", "e.txt", "f.txt"]
+    print("***** *** greedy ***** ***")
+    for file in files:
+        print(file)
+        instance = inputreader3.readInput("example-graphs/{}".format(file))
+        schedules_greedy = instance.greedy()
+        start_time = time.time()
+        score = instance.simulate(schedules_greedy)
+        print(score)
+        print("\ttime elapsed: {}".format(time.time() - start_time))
+
+    print("\n***** *** intelligent uniform ***** ***")
+    for file in files:
+        print(file)
+        instance = inputreader3.readInput("example-graphs/{}".format(file))
+        schedules_greedy = instance.intelligent_uniform_schedules()
+        start_time = time.time()
+        score = instance.simulate(schedules_greedy)
+        print(score)
+        print("\ttime elapsed: {}".format(time.time() - start_time))
+
     return 0
 
 
